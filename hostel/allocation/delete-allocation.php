07e5cd6dd115174087e260414831e54203caa016
eren238 <oludotunayomiposi5@gmail.com>
2026-09-23 21:01:01 +0100

    this file was updatted on 23/09/2026 @ 9:01pm

---
+++ hostel/allocation/delete-allocation.php
@@ -0,0 +1,36 @@
+<?php require_once __DIR__ . '/../config/connection.php'; ?>
+
+<?php
+$allocationId = trim($_POST['allocationId'] ?? '');
+
+if ($allocationId == '') {
+    $response = [
+        'success' => false,
+        'message' => 'ALLOCATION ID REQUIRED'
+    ];
+    goto end;
+}
+
+$checkAlloc = mysqli_query($conn, "SELECT * FROM allocation_tab WHERE allocation_id = '$allocationId'") or die(mysqli_error($conn));
+if (mysqli_num_rows($checkAlloc) == 0) {
+    $response = [
+        'success' => false,
+        'message' => 'ALLOCATION NOT FOUND'
+    ];
+    goto end;
+}
+
+$allocData = mysqli_fetch_assoc($checkAlloc);
+$bedId = $allocData['bed_id'];
+
+mysqli_query($conn, "UPDATE `beds_tab` SET `status_id` = 'D', `student_id` = NULL, `updated_at` = NOW() WHERE `bed_id` = '$bedId'") or die(mysqli_error($conn));
+mysqli_query($conn, "DELETE FROM `allocation_tab` WHERE `allocation_id` = '$allocationId'") or die(mysqli_error($conn));
+
+$response = [
+    'success' => true,
+    'message' => 'ALLOCATION DELETED AND BED FREED SUCCESSFULLY'
+];
+
+end:
+echo json_encode($response);
+?>
